5996529d442a160c263caeded8c4991ae9542fb1
bbosmedia <bbosmedia@users.noreply.github.com>
2022-02-18 18:47:08 +0500

    Ready responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const input = document.querySelector('.header__input-input')
 const form = document.querySelector('.header__input')
 
+const menuToggle = document.querySelector('.menu-toggle')
+const navigation = document.querySelector('.navigation')
+
 input.addEventListener('focus', () => {
 	form.classList.add('active')
+})
+
+menuToggle.addEventListener('click', ()=>{
+	menuToggle.classList.toggle('active')
+	navigation.classList.toggle('active')
 })
